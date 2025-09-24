acf84e23e7b952825359e43491e3cbc2f3bf4358
Jenas Anton <jenasav010@gmail.com>
2025-09-24 15:15:54 +0530

    done for contribution

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,25 @@
 "use client"
 import Experience from "./Components/Experience";
-import { UI } from "./Components/UI";
 import styles from "./Page.module.css";
-
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { Book } from "./Components/Book";
+import { pictures } from "./data";
 export default function Page() {
   return (
     <>
       <div className={styles.background} />
 
       <main >
-        {/* <UI /> commented */}
-        <UI/>
-        <Experience />
+          <Canvas
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 5, 2]} intensity={1} />
+      <Book pictures= {pictures} position={[0, 0, 0]} />
+      <OrbitControls enableZoom enablePan={false} />
+      <Environment preset="studio" />
+    </Canvas>
       </main>
     </>
   );
