a4cf3cd7377c0486ae0ffc20c8a32d2a1352d41c
Jenas Anton <jenasav010@gmail.com>
2025-09-29 00:19:24 +0530

    new background

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,12 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
-      >
-        {children}
-      </body>
-
+    <body className={`${geistSans.variable} ${geistMono.variable} antialiased text-foreground`}>
+      {children}
+    </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,10 @@ import { pictures } from "./data";
 export default function Page() {
   return (
     <>
-      <div className={styles.background} />
-
-      <main >
-          <Canvas
-      style={{ width: "100vw", height: "100vh" }}
-    >
+      <main>
+        <Canvas
+          style={{ width: "100vw", height: "100vh" }}
+        >
       <ambientLight intensity={1} />
       {/* <directionalLight position={[0 , 0 , 2]} intensity={2} /> */}
       <Book pictures= {pictures} position={[0, 0, 0]} />
